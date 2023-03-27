cd11a1d51d467a07918156e5383e99234258b898
ivan1arriola <ivan.arriola@fing.edu.uy>
2023-03-27 20:28:47 -0300

    Experiencias

---
+++ src/Cuerpo/Components/ExperienciasCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+interface CardProps {
+  titulo: string;
+  periodo: string;
+  texto: string;
+  link: string;
+}
+
+const ExperienciaCard: React.FC<CardProps> = ({ titulo, periodo, texto, link }) => {
+  return (
+    <Card className="h-100">
+      <Card.Body>
+        <Card.Title>{titulo}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{periodo}</Card.Subtitle>
+        <Card.Text>{texto}</Card.Text>
+        <Card.Link href={link}>Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ExperienciaCard;
--- src/Cuerpo/Experiencias.tsx
+++ src/Cuerpo/Experiencias.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import CustomCard from './Components/ExperienciasCard';
 
 const Experiencias = () => {
-    return (
-        <Container>
-            <h1>Experiencias</h1>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Row>
+        <h1>Experiencias</h1>
+      </Row>
+      <Row>
+        <Col>
+          <CustomCard
+            titulo="Floreria Camelia"
+            periodo="Actualidad"
+            texto="Desarrollo de la pagina estatica de Floreria Camelia"
+            link="https://www.floreriacamelia.com"
+          />
+        </Col>
+
+        <Col>
+          <CustomCard
+            titulo="Proyecto JAP"
+            periodo="2022"
+            texto="Proyecto de Desarrollo Web del curso de Jovenes a Programar - CEIBAL"
+            link="https://ivan1arriola.github.io/ProyectoJAP/"
+          />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Experiencias;
