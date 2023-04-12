f52f8deb20ccff0f7268662ac61175ec940715bb
ivan1arriola <ivan.arriola@fing.edu.uy>
2023-04-11 23:34:35 -0300

    actualizacion en proyectos

--- src/pages/Proyectos.tsx
+++ src/pages/Proyectos.tsx
@@ -15,20 +15,20 @@ const Proyectos = () => {
           titulo="Floreria Camelia"
           imagen="https://www.floreriacamelia.com/floreriaCamelia.svg"
           periodo="Actualidad"
-          texto="Desarrollo de la pagina estatica de Floreria Camelia"
+          texto="Desarrollo de la pagina estatica de Floreria Camelia. Esta pagina esta hecha con React y Javascript"
           link="https://www.floreriacamelia.com"
         />
         <CustomCard
           titulo='Floreria Camelia - Admin'
           periodo='Actualidad'
-          texto='Desarrollo de la pagina dedicada a la administracion de la pagina de Floreria Camelia'
+          texto='Desarrollo de la API dedicada a modificar de forma dinamica las imagenes de la pagina de Floreria Camelia. La API esta hecha con Phyton y en su raiz tiene una pagina hecha con HTML y Bootstrap.'
           link='https://admin.floreriacamelia.com'
         />
         <CustomCard
           titulo="Proyecto JAP"
           imagen="https://ivan1arriola.github.io/ProyectoJAP/img/login.png"
           periodo="2022"
-          texto="Proyecto de Desarrollo Web del curso de Jovenes a Programar - CEIBAL"
+          texto="Proyecto de Desarrollo Web del curso de Jovenes a Programar - CEIBAL. La pagina se hizo utilizando CSS, HTML y Javascript"
           link="https://ivan1arriola.github.io/ProyectoJAP/"
         />
       </Container>
