316930d08bae87fa9fe085dc46c389c8a380c08e
ivan1arriola <ivan.arriola@fing.edu.uy>
2023-04-11 21:26:39 -0300

    new footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,33 @@
 import styled from "styled-components";
 
 const FooterContainer = styled.footer`
-  background-color: ${props => props.theme.colors.background };
+  background-color: ${props => props.theme.colors.pageBackground };
   color: ${props => props.theme.colors.white};
   text-align: center;
-    padding: 10px;
-    margin: 0 10px 0 10px;
+  padding: 10px;
+  margin: 0;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 9999;
+  transition: opacity 0.3s ease-in-out;
+  opacity: 1;
+  
+  &:hover {
+    opacity: 0;
+    display: none;
+  }
 `;
 
 const FooterText = styled.span`
-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.primary};
   font-size: 16px;
-  margin: auto;
 `;
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <div>
-        <FooterText>Ivan Arriola - Portfolio</FooterText>
-      </div>
+      <FooterText>Ivan Arriola - Portfolio</FooterText>
     </FooterContainer>
   );
 };
